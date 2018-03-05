bf6798395ee9080d828ddf3f1ec15842bef741ab
whatsondoc <30378443+whatsondoc@users.noreply.github.com>
2018-03-05 01:19:15 -0500

    Adding first script to test deployments of Azure VMSS (creating an RG, provisioning of resources, deprovisioning of resources and deletion of the RG).

---
+++ vmss-creation.ps1
@@ -0,0 +1,124 @@
+if ( (test-path -path azuredeploy.json) -and (test-path -path *parameters.json) )
+{
+$RNDNAME = (-join ((48..57) + (97..122) | Get-Random -Count 8 | % {[char]$_}))
+$OUTPUT = $RNDNAME + "-output"
+
+Start-Transcript -path $OUTPUT -append
+
+write-output "`n{{{-------------------------------------------|-/-/-/-^-\-\-\-|-------------------------------------------}}}`n"
+write-output "`nOpening testing framework: Microsoft Azure Virtual Machine Scale Set (VMSS)`n"
+
+
+$LOCATION = "northcentralus"
+
+write-output "Our random string for this deployment: $RNDNAME"
+
+
+function create-rg {
+write-output "`n
+###########################################
+## 0%-->25%: Create Azure Resource Group ##
+###########################################
+"
+
+$STARTCREATERG = (get-date -displayhint Time)
+write-output "`nCreating Azure Resource Group..."
+
+New-AzureRmResourceGroup -Location $LOCATION -Name $RNDNAME-rg 
+
+$ENDCREATERG = (get-date -displayhint Time)
+
+$TSCREATERG = ([datetime]"$ENDCREATERG" -[datetime]"$STARTCREATERG")
+"`nThe time taken to create the Azure Resource Group '$RNDNAME-rg': {0:c}" -f $TSCREATERG
+
+}
+
+function create-deployment {
+write-output "`n
+###########################################
+## 25%-->50%: Create template deployment ##
+###########################################
+" 
+
+# You will need the deployment templates on the local machine called 'azuredeploy.json' & '<something>.parameters.json'
+# Initiating a deployment from a local set of deployment template & parameter files to the above RG:
+
+$STARTDEPLOYMENTCREATE = (get-date -displayhint Time)
+write-output "`nStarting deployment from template files provided...`n"
+
+New-AzureRmResourceGroupDeployment `
+-TemplateFile ./azuredeploy.json `
+-TemplateParameterFile ./azuredeploy.parameters.json `
+-Name $RNDNAME-deployment `
+-ResourceGroupName $RNDNAME-rg 
+
+$ENDDEPLOYMENTCREATE = (get-date -displayhint Time)
+
+write-output "`nCapturing details of the deployment operations:"
+
+(Get-AzureRmResourceGroupDeploymentOperation -ResourceGroupName $RNDNAME-rg -DeploymentName $RNDNAME-deployment).Properties | select-object provisioningoperation,duration,targetresource | Format-list
+
+$TSDEPLOYMENT = ([datetime]"$ENDDEPLOYMENTCREATE" -[datetime]"$STARTDEPLOYMENTCREATE")
+"The time taken to complete the template deployment '$RNDNAME-deployment': {0:c}" -f $TSDEPLOYMENT
+
+}
+
+
+function deprovision-vmss {
+write-output "`n
+######################################
+## 50%-->75%: Deprovision Resources ##
+######################################
+" 
+
+write-output "`nDeprovisioning the VMSS to 0 nodes..."
+$STARTDEPROVISIONVMSS = (get-date -displayhint Time)
+
+$vmssName = (get-azurermvmss -ResourceGroupName $RNDNAME-rg).Name
+$vmss = (get-azurermvmss -ResourceGroupName $RNDNAME-rg -VMScaleSetName $vmssName)
+$vmss.sku.capacity = 0
+
+Update-AzureRmVmss -ResourceGroupName $RNDNAME-rg -Name vmss -VirtualMachineScaleSet $vmss | out-null
+
+$ENDDEPROVISIONVMSS = (get-date -displayhint Time)
+write-output "Deprovisioning completed: $vmssName`n" 
+
+$TSDEPROVISION = ([datetime]"$ENDDEPROVISIONVMSS" -[datetime]"$STARTDEPROVISIONVMSS")
+"`nThe time taken to deprovision $vmssName nodes to 0: {0:c}" -f $TSDEPROVISION
+
+}
+
+
+function delete-rg {
+write-output "`n
+##########################################################
+## 75%-->100%: Deleting Resource Group and its contents ##
+##########################################################
+" 
+
+write-output "`nNow deleting the Azure Resource Group created for this exercise: $RNDNAME-rg..."
+write-output "`nIf 'True', the Resource Group has been successfully deleted:"
+
+$STARTDELETERG = (get-date -displayhint Time)
+
+remove-azurermresourcegroup -resourcegroupname $RNDNAME-rg -force
+
+$ENDDELETERG = (get-date -displayhint Time)
+
+$TSDELETERG = ([datetime]"$ENDDELETERG" -[datetime]"$STARTDELETERG")
+"`nThe time taken to delete the Resource Group $RNDNAME-rg: {0:c}" -f $TSDELETERG
+
+echo "`n"
+
+}
+
+create-rg
+create-deployment
+deprovision-vmss
+delete-rg
+
+Stop-Transcript
+
+echo "`n"
+
+}
