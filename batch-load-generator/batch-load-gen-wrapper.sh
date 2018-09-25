d02f2aa2eb222da33ad2618351421838d4d49bb5
whatsondoc <30378443+whatsondoc@users.noreply.github.com>
2018-09-25 16:35:56 -0400

    Additional modifications.
    Changing from using autoscale to a defined number of VMs relative to the number of tasks requested.

--- batch-load-generator/batch-load-gen-wrapper.sh
+++ batch-load-generator/batch-load-gen-wrapper.sh
@@ -48,14 +48,25 @@ Such tests could include storage performance tests or saturating connection coun
 
 ::SYNTAX::
 
-    $ /path/to/batch-load-gen-wrapper.sh -j <JOB_NAME> -p <POOL_ID> -t <NUMBER_OF_TASKS> -n <URL_OF_NODE_NODE_CFG> -w <URL_OF_JOB_NODE_CFG>
+\t$ /path/to/batch-load-gen-wrapper.sh -j <JOB_NAME> -p <POOL_ID> -t <NUMBER_OF_TASKS> -n <URL_OF_NODE_CONFIG> -w <URL_OF_JOB_CONFIG>
 
-    $ /path/to/batch-load-gen-wrapper.sh \
-    -j SEQUENTIAL_READ \                        # Job name
-    -p GLUSTER_FS_01 \                          # Pool ID
-    -t 39 \                                     # Tasks
-    -n http://aka.ms/glusterfs-nodesetup.sh     # Node config
-    -w http://aka.ms/glusterfs-perftest.sh      # Job config
+\t$ /path/to/batch-load-gen-wrapper.sh \
+\t-j SEQUENTIAL_READ \ \t\t# Job name
+\t-p GLUSTER_FS_01 \ \t\t# Pool ID
+\t-t 39 \ \t\t# Tasks
+\t-n http://aka.ms/glusterfs-nodesetup.sh \t\t# Node config
+\t-w http://aka.ms/glusterfs-perftest.sh \t\t# Job config
+
+The default VM size is Standard_D1_v2. If you want to modify the size of a VM on which these tasks run, utilise the '-s' switch and specify a valid VM type. For example:
+
+\t$ /path/to/batch-load-gen-wrapper.sh \
+\t-j SEQUENTIAL_READ \ \t\t# Job name
+\t-p GLUSTER_FS_01 \ \t\t# Pool ID
+\t-s Standard_F8 \ \t\t# Azure VM size <<<<
+\t-t 39 \ \t\t# Tasks
+\t-n http://aka.ms/glusterfs-nodesetup.sh \t\t# Node config
+\t-w http://aka.ms/glusterfs-perftest.sh \t\t# Job config
+
 \n\n"
 
     exit 1
@@ -63,7 +74,7 @@ Such tests could include storage performance tests or saturating connection coun
 
 testsetup
 
-while getopts :j:p:t:n:w: option
+while getopts :j:p:t:n:w:s option
 do 
 case "$option"
 in
@@ -72,11 +83,17 @@ in
     t) TASK_NUM=${OPTARG} ;;
     n) NODE_CFG="${OPTARG}" ;;
     w) JOB_CFG="${OPTARG}" ;;
+    s) VM_SIZE="${OPTARG}" ;;
     *) help ;;
 esac
 done
 
 # Collecting Batch credentials from the accompanying credentials file
+if cat batch.creds | grep "ACCOUNT" > /dev/null ]
+then 
+    echo -e "You need to update the batch.creds file to include your specific Batch account details. \n\nExiting...\n"
+    exit 1
+fi
 BATCH_ACCT=$(cat batch.creds | grep BATCH_ACCT | awk '{print $2}')
 BATCH_ENDPOINT=$(cat batch.creds | grep BATCH_ENDPOINT | awk '{print $2}')
 BATCH_KEY=$(cat batch.creds | grep BATCH_KEY | awk '{print $2}')
@@ -88,7 +105,13 @@ then
 
     sed -i "s#POOL_ID_NULL#${POOL_ID}#g" ${POOL_ID}-batch-client-pool.json
     sed -i "s#NODE_CFG_NULL#${NODE_CFG}#g" ${POOL_ID}-batch-client-pool.json
-    
+    sed -i "s#TASK_NUM_NULL#${TASK_NUM}#g" ${POOL_ID}-batch-client-pool.json
+
+    if [ ! -z $VM_SIZE ]
+    then
+        sed -i "s#Standard_D1_v2#${VM_SIZE}#g" ${POOL_ID}-batch-client-pool.json
+    fi
+
     az batch pool create --account-name ${BATCH_ACCT} --account-endpoint ${BATCH_ENDPOINT} --account-key ${BATCH_KEY} --template ${POOL_ID}-batch-client-pool.json
 fi
 
